24c8d7f51545bc0af16457cdc7079a22f13e20a1
ranjithRK01 <ranjithsumathi22498@gmail.com>
2021-09-10 07:12:34 -0700

    deploy-path-issue

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + './dist/BlogFreeProjectPublic'));
+app.use(express.static(__dirname + './dist/blog-free-project'));
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'./dist/BlogFreeProjectPublic/index.html'));
+  res.sendFile(path.join(__dirname+'./dist/blog-free-project/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080,()=>{
+  console.log("App listen port ",8080)
+});
