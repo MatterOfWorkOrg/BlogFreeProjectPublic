5d2de3ff13e59c6be7c653a5518f318d73192f9d
ranjithRK01 <ranjithsumathi22498@gmail.com>
2021-09-10 07:55:51 -0700

    deploy-issue

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/blog-free-project'));
+app.use(express.static('./dist/blogFreeProject'));
 
 app.get('/*', function(req,res) {
-  res.sendFile('index.html', { root : 'dist/blog-free-project/'});
+  res.sendFile('index.html', { root : 'dist/blogFreeProject/'});
 });
 
 // Start the app by listening on the default Heroku port
